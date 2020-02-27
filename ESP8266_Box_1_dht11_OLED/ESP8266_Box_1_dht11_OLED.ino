4886f21313d7b9c862caae17cd30fbbbb599ecda
Anton Belov <antonioshch14@gmail.com>
2020-02-27 20:58:10 +0300

    Add project files.

---
+++ ESP8266_Box_1_dht11_OLED/ESP8266_Box_1_dht11_OLED.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP8266_Box_1_dht11_OLED.ino
+ Created:	2020-02-27 20:57:49
+ Author:	z003pr4u
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
